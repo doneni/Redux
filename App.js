7e4beb93c51dbf1ffbb5ee6b7148f93f3e03739d
doneni <xxerrenn@icloud.com>
2022-09-14 19:03:27 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default App;
---
+++ app/components/CounterList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    ScrollView,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
